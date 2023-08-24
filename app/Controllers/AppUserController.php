8e6fef86a12229b36952d20a4b5645f246a1c1aa
YanBerdin <yandevformation@gmail.com>
2023-08-24 03:41:21 +0200

    AppUserController First step

--- app/Controllers/AppUserController.php
+++ app/Controllers/AppUserController.php
@@ -3,16 +3,35 @@
 namespace App\Controllers;
 
 use App\Models\AppUser;
+use App\Controllers\CoreController;
 
 class AppUserController extends CoreController
 {
+    // Affichage du formulaire
     public function login()
     {
         $this->show('user/login');
     }
 
+    // Traitement du formulaire
+    // public function loginUser()
+    // {
+        // dump($_POST);
+    // }
+
     public function loginUser()
     {
-        // dump($_POST);
+        dump($_POST);
+
+        if (!empty($_POST)) {
+            // Manière de récupérer les données simplement via $_POST
+            // $email = $_POST['email'];
+            // $password = $_POST['password'];
+
+            // On récupère les données et on passe par filter_input
+            $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+            $password = filter_input(INPUT_POST, 'password');
+
+        }
     }
 }
